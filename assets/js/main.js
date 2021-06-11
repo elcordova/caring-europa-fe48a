867bb45b61829ca9d38e4da56b6738aa724f938d
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 05:35:50 +0000

    main.js: changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,13 +17,13 @@ jQuery(document).ready(function($) {
 
         $(".btn").click(function() {
             const wrapper = $(".wrapper")[0];
-            var opt = {
-  margin:       1,
-  filename:     'myfile.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { scale: 2 },
-  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-};
+            var set = {
+                margin:       1,
+                filename:     'myfile.pdf',
+                image:        { type: 'jpeg', quality: 0.98 },
+                html2canvas:  { scale: 2 },
+                jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+                };
             html2pdf().from(wrapper).save('elcordova.pdf');
         });
 
