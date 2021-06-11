286e007457e1918fb649b768e551909fdd50e383
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 05:23:11 +0000

    main.js: changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@ jQuery(document).ready(function($) {
         });
 
 
-        $( "#download" ).click(function() {
-  alert( "Handler for .click() called." );
-});
+        // $( "#download" ).click(function() {
+            alert( "Handler for .click() called." );
+        // });
 
     });
    
