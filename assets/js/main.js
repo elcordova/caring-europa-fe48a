cea9c57cd16c64ad38eb50cace424b32aa5cce51
Stackbit Code Editor <projects@stackbit.com>
2021-06-11 05:28:44 +0000

    main.js: changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($) {
 
         $(".btn").click(function() {
             const wrapper = $(".wrapper");
-            html2pdf(wrapper).save();
+            html2pdf().from(wrapper).save();
         });
 
     });
